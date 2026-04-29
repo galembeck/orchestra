a835d84b7dfb71933121d2fe9ed4197cea7e1081
Pedro Galembeck <galembeckpedro@gmail.com>
2026-04-29 19:04:06 -0300

    build: update & customize packages dependencies and configurations

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,8 +1,10 @@
-import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import turboPlugin from "eslint-plugin-turbo";
-import tseslint from "typescript-eslint";
-import onlyWarn from "eslint-plugin-only-warn";
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import onlyWarn from 'eslint-plugin-only-warn';
+import sortExportsPlugin from 'eslint-plugin-sort-exports';
+import turboPlugin from 'eslint-plugin-turbo';
+import tseslint from 'typescript-eslint';
 
 /**
  * A shared ESLint configuration for the repository.
@@ -16,9 +18,12 @@ export const config = [
   {
     plugins: {
       turbo: turboPlugin,
+      'sort-exports': sortExportsPlugin,
     },
     rules: {
-      "turbo/no-undeclared-env-vars": "warn",
+      'turbo/no-undeclared-env-vars': 'warn',
+      'sort-exports/sort-exports': ['warn', { ignoreCase: true, sortExportKindFirst: 'type' }],
+      complexity: 'warn',
     },
   },
   {
@@ -27,6 +32,77 @@ export const config = [
     },
   },
   {
-    ignores: ["dist/**"],
+    ignores: ['dist/**'],
+  },
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'import/order': [
+        'warn',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+          pathGroups: [
+            {
+              pattern: '@src/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@app/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@mblabs-roccato-frontend/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@pages/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: 'next',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: 'next/**',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: 'mblabs-roccato-frontend-commons/**',
+              group: 'external',
+              position: 'before',
+            },
+
+            {
+              pattern: '**/*.+(css)',
+              group: 'object',
+              patternOptions: { matchBase: true },
+              position: 'after',
+            },
+          ],
+          'newlines-between': 'always',
+          pathGroupsExcludedImportTypes: ['builtin'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'import/no-duplicates': 'warn',
+      'import/no-unresolved': 'off',
+      'import/extensions': 'off',
+    },
   },
 ];
