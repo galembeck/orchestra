63c79a832c1a63e78c29e7ebaa2048fa60a77cf3
Pedro Galembeck <pedro.galembeck@mblabs.com.br>
2026-05-16 17:04:01 -0300

    feat(api-admin): register new admin-users and admin-roles routes

--- apps/api-admin/src/http/routes/index.ts
+++ apps/api-admin/src/http/routes/index.ts
@@ -1,5 +1,10 @@
+// apps/api-admin/src/http/routes/index.ts
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { getAdminRolesRoute } from "./admin-roles/get-admin-roles.js";
 import { createAdminUserRoute } from "./admin-users/create-admin-user.js";
+import { getAdminUsersRoute } from "./admin-users/get-admin-users.js";
+import { updateAdminUserPermissionsRoute } from "./admin-users/update-admin-user-permissions.js";
+import { updateAdminUserRoleRoute } from "./admin-users/update-admin-user-role.js";
 import { signInRoute } from "./auth/sign-in.js";
 import { approveCompanyRoute } from "./companies/approve-company.js";
 import { getCompanyDocumentsRoute } from "./companies/get-company-documents.js";
@@ -14,6 +19,10 @@ export const routes: FastifyPluginAsyncZod = async (app) => {
 	await app.register(signInRoute);
 	await app.register(getMeRoute);
 	await app.register(createAdminUserRoute);
+	await app.register(getAdminUsersRoute);
+	await app.register(updateAdminUserRoleRoute);
+	await app.register(updateAdminUserPermissionsRoute);
+	await app.register(getAdminRolesRoute);
 	await app.register(getCompanyRegistrationsRoute);
 	await app.register(getCompanyRegistrationRoute);
 	await app.register(getCompanyDocumentsRoute);
